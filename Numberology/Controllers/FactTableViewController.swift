cab605fbc72b706bcb1ab4d13a1312748ae207a2
Anton Petrov <beavean@gmail.com>
2023-04-10 15:09:10 +0300

    feat: Add FactTableViewController and FactTableViewCell with configuration

---
+++ Numberology/Controllers/FactTableViewController.swift
@@ -0,0 +1,95 @@
+//
+//  FactTableViewController.swift
+//  Numberology
+//
+//  Created by Beavean on 07.04.2023.
+//
+
+import SnapKit
+import UIKit
+
+final class FactTableViewController: UITableViewController {
+    // MARK: - Properties
+    private var sortedData: [(key: String, value: String)] = []
+
+    // MARK: - Lifecycle
+
+    init(data: [String: String]) {
+        super.init(nibName: nil, bundle: nil)
+        sortedData = data.sorted { $0.key < $1.key }
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .mainFillColor
+        configureTableView()
+        configureNavigationBar()
+    }
+
+    override func viewDidLayoutSubviews() {
+        updateScrollingEnabled()
+    }
+
+    // MARK: - Actions
+
+    @objc private func backButtonTapped() {
+        navigationController?.popViewController(animated: true)
+    }
+
+    // MARK: - Configuration
+
+    private func configureTableView() {
+        tableView.register(FactTableViewCell.self, forCellReuseIdentifier: FactTableViewCell.reuseIdentifier)
+        tableView.rowHeight = UITableView.automaticDimension
+        tableView.allowsSelection = false
+        tableView.separatorStyle = .none
+    }
+
+    private func configureNavigationBar() {
+        let appearance = UINavigationBarAppearance()
+        appearance.configureWithOpaqueBackground()
+        appearance.backgroundColor = .mainFillColor
+        appearance.shadowColor = .clear
+        navigationController?.setNavigationBarHidden(false, animated: true)
+        navigationController?.navigationBar.standardAppearance = appearance
+        navigationController?.navigationBar.scrollEdgeAppearance = appearance
+        navigationItem.hidesBackButton = true
+        let backButton = UIBarButtonItem(image: Constants.Images.close.image,
+                                         style: .plain,
+                                         target: self,
+                                         action: #selector(backButtonTapped))
+        backButton.tintColor = .textOnFilledBackgroundColor
+        navigationItem.rightBarButtonItem = backButton
+    }
+
+    // MARK: - Helpers
+
+    private func updateScrollingEnabled() {
+        if tableView.contentSize.height <= tableView.bounds.size.height {
+            tableView.isScrollEnabled = false
+        } else {
+            tableView.isScrollEnabled = true
+        }
+    }
+
+    // MARK: - Table view data source
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return sortedData.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: FactTableViewCell.reuseIdentifier,
+                                                       for: indexPath)
+                as? FactTableViewCell
+        else { return UITableViewCell() }
+        let title = sortedData[indexPath.row].key
+        let text = sortedData[indexPath.row].value
+        cell.configure(withTitle: title, text: text)
+        return cell
+    }
+}
---
+++ Numberology/Views/FactTableViewCell.swift
@@ -0,0 +1,60 @@
+//
+//  FactTableViewCell.swift
+//  Numberology
+//
+//  Created by Beavean on 07.04.2023.
+//
+
+import SnapKit
+import UIKit
+
+final class FactTableViewCell: UITableViewCell {
+    // MARK: - UI Elements
+
+    private let titleLabel = CustomLabel(fontSize: 28,
+                                         isBold: true,
+                                         textAlignment: .center,
+                                         textColor: .textOnFilledBackgroundColor)
+    private let descriptionLabel = CustomLabel(fontSize: 16,
+                                               isBold: true,
+                                               textAlignment: .center,
+                                               textColor: .textOnFilledBackgroundColor)
+
+    // MARK: - Properties
+
+    static let reuseIdentifier = String(describing: FactTableViewCell.self)
+
+    // MARK: - Initialization
+
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+          super.init(style: style, reuseIdentifier: reuseIdentifier)
+        configureUI()
+      }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    // MARK: - Configuration
+
+    private func configureUI() {
+        backgroundColor = .clear
+        addSubview(titleLabel)
+        titleLabel.snp.makeConstraints {
+            $0.left.right.top.equalToSuperview().inset(Constants.StyleDefaults.outerPadding)
+        }
+
+        addSubview(descriptionLabel)
+        descriptionLabel.snp.makeConstraints {
+            $0.top.equalTo(titleLabel.snp.bottom).offset(Constants.StyleDefaults.verticalPadding)
+            $0.left.equalTo(titleLabel.snp.left)
+            $0.right.equalTo(titleLabel.snp.right)
+            $0.bottom.equalToSuperview().inset(Constants.StyleDefaults.outerPadding)
+        }
+    }
+
+    func configure(withTitle title: String, text: String) {
+        titleLabel.text = title
+        descriptionLabel.text = text
+    }
+}
