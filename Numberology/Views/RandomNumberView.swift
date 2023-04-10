d695eb4ce4cfede189b8259506ff42921d314e2b
Anton Petrov <beavean@gmail.com>
2023-04-10 15:06:24 +0300

    feat: Add supplementary views for NumberInputContainer

---
+++ Numberology/Views/RandomNumberView.swift
@@ -0,0 +1,49 @@
+//
+//  RandomNumberView.swift
+//  Numberology
+//
+//  Created by Beavean on 05.04.2023.
+//
+
+import SnapKit
+import UIKit
+
+final class RandomNumberView: UIView {
+    // MARK: - UI Elements
+
+    private let randomNumberLabel = CustomLabel(text: """
+                                                      Tap on Display Fact button
+                                                      to get a number with random available fact
+                                                      """,
+                                                fontSize: 16,
+                                                isBold: true,
+                                                textAlignment: .center)
+
+    // MARK: - Initialization
+
+    init() {
+        super.init(frame: .zero)
+        configureUI()
+    }
+
+    @available(*, unavailable)
+    required init?(coder _: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    // MARK: - Configuration
+
+    private func configureUI() {
+        addSubview(randomNumberLabel)
+        randomNumberLabel.snp.makeConstraints {
+            $0.height.equalTo(Constants.StyleDefaults.itemHeight)
+            $0.left.right.top.equalToSuperview()
+        }
+    }
+}
+
+extension RandomNumberView: NumberInputContainer {
+    func getNumbers() -> [Int] {
+        return [0]
+    }
+}
---
+++ Numberology/Views/UserNumberView.swift
@@ -0,0 +1,56 @@
+//
+//  UserNumberView.swift
+//  Numberology
+//
+//  Created by Beavean on 05.04.2023.
+//
+
+import SnapKit
+import UIKit
+
+final class UserNumberView: UIView {
+    // MARK: - UI Elements
+
+    private lazy var userNumberTextField = NumberInputTextField(placeholder: textFieldPlaceholder)
+
+    // MARK: - Properties
+
+    private let textFieldPlaceholder: String = {
+        let numberFormatter = NumberFormatter()
+        numberFormatter.numberStyle = .decimal
+        numberFormatter.groupingSeparator = " "
+        numberFormatter.locale = Locale.current
+        if let formattedMaxNumber = numberFormatter.string(from: NSNumber(value: Constants.APIDefaults.maxNumber)) {
+            return "Input any number up to \(formattedMaxNumber)"
+        }
+        return "Input any number"
+    }()
+
+    // MARK: - Initialization
+
+    init() {
+        super.init(frame: .zero)
+        configureUI()
+    }
+
+    @available(*, unavailable)
+    required init?(coder _: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    // MARK: - Configuration
+
+    private func configureUI() {
+        addSubview(userNumberTextField)
+        userNumberTextField.snp.makeConstraints {
+            $0.height.equalTo(Constants.StyleDefaults.itemHeight )
+            $0.left.right.top.equalToSuperview()
+        }
+    }
+}
+
+extension UserNumberView: NumberInputContainer {
+    func getNumbers() -> [Int] {
+        userNumberTextField.getInputtedInts()
+    }
+}
