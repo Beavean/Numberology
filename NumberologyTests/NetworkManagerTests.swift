72469454c4d1e781b623942998fc9d025df8dbcf
Anton Petrov <beavean@gmail.com>
2023-04-14 19:48:28 +0300

    feat: Add NetworkManager unit tests

---
+++ NumberologyTests/NetworkManagerTests.swift
@@ -0,0 +1,130 @@
+//
+//  NetworkManagerTests.swift
+//  NumberologyTests
+//
+//  Created by Beavean on 14.04.2023.
+//
+
+@testable import Numberology
+import XCTest
+
+final class NetworkManagerTests: XCTestCase {
+    private var sut: NetworkManager!
+
+    override func setUp() {
+        super.setUp()
+        sut = NetworkManager()
+    }
+
+    override func tearDown() {
+        super.tearDown()
+        sut = nil
+    }
+
+    func testSingleNumberFactFetch() {
+        let expectation = expectation(description: #function)
+        var receivedFacts = [NumberFact]()
+        sut.fetchNumbersFacts(numbers: [1337]) { result in
+            switch result {
+            case .success(let facts):
+                receivedFacts = facts
+            case .failure(let error):
+                XCTFail("Error fetching numbers info: \(error)")
+            }
+            expectation.fulfill()
+        }
+        waitForExpectations(timeout: 3) { error in
+            if error != nil {
+                XCTFail("Expectation timeout error")
+            } else {
+                XCTAssertFalse(receivedFacts.isEmpty, "Facts array should not be empty")
+            }
+        }
+    }
+
+    func testMultipleNumberFactsFetch() {
+        let expectation = expectation(description: #function)
+        var receivedFacts = [NumberFact]()
+        sut.fetchNumbersFacts(numbers: [1337, 999, 0, 1]) { result in
+            switch result {
+            case .success(let facts):
+                receivedFacts = facts
+            case .failure(let error):
+                XCTFail("Error fetching numbers info: \(error)")
+            }
+            expectation.fulfill()
+        }
+        waitForExpectations(timeout: 3) { error in
+            if error != nil {
+                XCTFail("Expectation timeout error")
+            } else {
+                XCTAssertFalse(receivedFacts.isEmpty, "Facts array should not be empty")
+            }
+        }
+    }
+
+    func testRangeOfNumbersFactsFetch() {
+        let expectation = expectation(description: #function)
+        var receivedFacts = [NumberFact]()
+        let range = [1337, 2000]
+        sut.fetchFactsForNumbersIn(range: range) { result in
+            switch result {
+            case .success(let facts):
+                receivedFacts = facts
+            case .failure(let error):
+                XCTFail("Error fetching numbers info: \(error)")
+            }
+            expectation.fulfill()
+        }
+        waitForExpectations(timeout: 3) { error in
+            if error != nil {
+                XCTFail("Expectation timeout error")
+            } else {
+                XCTAssertFalse(receivedFacts.isEmpty, "Facts array should not be empty")
+            }
+        }
+    }
+
+    func testRandomNumberFactFetch() {
+        let expectation = expectation(description: #function)
+        var receivedFacts = [NumberFact]()
+        sut.fetchFactForRandomNumber { result in
+            switch result {
+            case .success(let facts):
+                receivedFacts = facts
+            case .failure(let error):
+                XCTFail("Error fetching numbers info: \(error)")
+            }
+            expectation.fulfill()
+        }
+        waitForExpectations(timeout: 3) { error in
+            if error != nil {
+                XCTFail("Expectation timeout error")
+            } else {
+                XCTAssertFalse(receivedFacts.isEmpty, "Facts array should not be empty")
+            }
+        }
+    }
+
+    func testDateFactFetch() {
+        let expectation = expectation(description: #function)
+        var receivedFacts = [DateFact]()
+        let dateAsArray = [12, 31]
+        sut.fetchDateFact(fromArray: dateAsArray) { result in
+            switch result {
+            case .success(let facts):
+                receivedFacts = facts
+            case .failure(let error):
+                XCTFail("Error fetching numbers info: \(error)")
+            }
+            expectation.fulfill()
+        }
+        waitForExpectations(timeout: 3) { error in
+            if error != nil {
+                XCTFail("Expectation timeout error")
+            } else {
+                XCTAssertFalse(receivedFacts.isEmpty, "Facts array should not be empty")
+            }
+        }
+    }
+}
